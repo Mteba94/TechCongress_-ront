404812b7ec192d6d1ab65f33fc80eba6cfbd3971
Mteba94 <mtebaland@umg.edu.gt>
2025-08-22 14:01:48 -0600

    correccion https

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     //api: 'https://localhost:44378/api/',
     //api: 'http://localhost/servicios/ws_congreso/api/'
     //api: 'http://localhost:81/api/'
-    api: 'http://www.congress.somee.com/api/'
+    api: 'https://www.congress.somee.com/api/'
 };
