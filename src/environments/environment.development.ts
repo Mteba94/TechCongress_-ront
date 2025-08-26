271987e0175e45d80f7cafaa1eb0be70838337f0
Mteba94 <mtebaland@umg.edu.gt>
2025-08-25 21:30:23 -0600

    user component

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    //api: 'https://localhost:44378/api/',
+    api: 'https://localhost:44378/api/',
     //api: 'http://localhost/servicios/ws_congreso/api/'
     //api: 'http://localhost:81/api/'
-    api: 'https://www.congress.somee.com/api/'
+    //api: 'https://www.congress.somee.com/api/'
 };
